457df1459164d8c29007722847f8eae6a99aeb0e
Shu-Nonaka <s.nonaka@soushin-lab.co.jp>
2021-07-27 18:06:32 +0900

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
-export function hello() {
-    return `hello!`;
-}
+import {app, BrowserWindow} from "electron"
+
+app.whenReady().then(() => {
+  const window = new BrowserWindow({
+    maxWidth: 1200,
+    maxHeight: 1200
+  });
+
+  window.loadFile('')
+})
