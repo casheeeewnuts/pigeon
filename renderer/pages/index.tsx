663c7c9a7101b32cf10ae7c5f96363a8366e4793
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-08-01 22:56:21 +0900

    WIP

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import {Card, Container, createStyles, Divider, Grid, IconButton, makeStyles, Paper, Theme} from "@material-ui/core";
-import {Dehaze} from "@material-ui/icons";
+import {
+    Card, CardHeader,
+    CardMedia,
+    Container,
+    createStyles,
+    Divider,
+    Grid,
+    IconButton,
+    makeStyles,
+    Paper,
+    Theme
+} from "@material-ui/core";
+import {Dehaze, MoreVert} from "@material-ui/icons";
 import Layout from '../components/Layout'
 
 import {useLoading} from "../hooks/useLoading";
 import {useSwitchableAside} from "../hooks/useSwitchableAside";
+import {BookCard} from "../components/book";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             flexGrow: 1,
-        },
-        paper: {
-            padding: theme.spacing(2),
-            textAlign: 'center',
-            color: theme.palette.text.secondary,
-            height: 100
         },
     }),
 );
@@ -35,29 +41,12 @@ const IndexPage = () => {
             </div>
             <div className={classes.root}>
                 <Grid container spacing={3}>
-                    <Grid container item xs={12} spacing={2}>
+                    <Grid container item xs={12} spacing={3}>
                         <Grid item xs={1}>
-                            <Card className={classes.paper}>item</Card>
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Card className={classes.paper}>item</Card>
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Card className={classes.paper}>item</Card>
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Card className={classes.paper}>item</Card>
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Card className={classes.paper}>item</Card>
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Card className={classes.paper}>item</Card>
-                        </Grid>
-                        <Grid item xs={1}>
-                            <Card className={classes.paper}>item</Card>
+                            <BookCard book={{title: '', cover: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'}} />
                         </Grid>
                     </Grid>
+                    <Divider/>
                 </Grid>
             </div>
             <Loading/>
